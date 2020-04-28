2ffa4d47a8eeec944af29c9981e55be90a6e9bd1
edisrud <edisrud@narrativescience.com>
2020-04-28 14:33:55 -0500

    fixed error in setup

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ import setuptools
 setuptools.setup(
     name='generic_behave',
     version=os.environ.get("BUILD_VERSION", "0.0.0.dev-1"),
-    packages=['test_utils.behave', 'test_utils.requests', 'test_utils.test_utils.selenium'],
+    packages=['test_utils.behave', 'test_utils.requests', 'test_utils.page_objects'],
     install_requires=open("requirements.txt").readlines(),
 )
