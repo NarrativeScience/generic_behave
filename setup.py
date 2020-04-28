aaf14f3d9bb3d7acc6602c647aaf2f0df70b32df
edisrud <edisrud@narrativescience.com>
2020-04-28 15:39:47 -0500

    fixed error in setup

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ import setuptools
 setuptools.setup(
     name='generic_behave',
     version=os.environ.get("BUILD_VERSION", "0.0.0.dev-1"),
-    packages=setuptools.find_packages(include=['ns_behave', 'ns_page_objects', 'ns_requests']),
+    py_modules=(['ns_behave', 'ns_page_objects', 'ns_requests']),
     install_requires=open("requirements.txt").readlines(),
 )
