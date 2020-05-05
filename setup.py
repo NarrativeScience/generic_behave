8e095653f33f5dc8280b413a50e5cdf378c7bd5d
edisrud <edisrud@narrativescience.com>
2020-05-05 10:18:38 -0500

    fixed setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import setuptools
 setuptools.setup(
     name="generic_behave",
     version=os.environ.get("BUILD_VERSION", "0.0.0.dev-1"),
-    packages={'generic_behave'},
+    packages=setuptools.find_packages(),
+    provides=setuptools.find_packages(),
     # install_requires=open("requirements.txt").readlines(),
 )
