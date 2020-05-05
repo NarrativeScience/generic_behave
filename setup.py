32e2b187689c18d84d702f6a5830caa659e14461
edisrud <edisrud@narrativescience.com>
2020-05-05 11:43:14 -0500

    uncommented line for requirements.txt

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setuptools.setup(
     version=os.environ.get("BUILD_VERSION", "0.0.0.dev-1"),
     packages=setuptools.find_packages(),
     provides=setuptools.find_packages(),
-    # install_requires=open("requirements.txt").readlines(),
+    install_requires=open("requirements.txt").readlines(),
 )
